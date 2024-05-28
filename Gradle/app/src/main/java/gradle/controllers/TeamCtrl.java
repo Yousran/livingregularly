f0138bf9646ef612d32a5e31d73ceb670695f089
Yousran <yusranmazidan@gmail.com>
2024-05-29 01:22:24 +0800

    FEAT: TeamView

--- Gradle/app/src/main/java/gradle/controllers/TeamCtrl.java
+++ Gradle/app/src/main/java/gradle/controllers/TeamCtrl.java
@@ -1,19 +1,23 @@
 package gradle.controllers;
 
 import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
 import gradle.config.DbConnect;
-import gradle.models.Team;
+import gradle.models.User;
 
 public class TeamCtrl extends DbConnect {
+
     public static boolean addTeam(String nama, int userId, int projekId) {
-        query = "INSERT INTO team (user_id, projek_id ,nama) VALUES (? , ?, ?)";
+        String query = "INSERT INTO team (user_id, projek_id ,nama) VALUES (? , ?, ?)";
         try {
             getConnection();
             preparedStatement = connection.prepareStatement(query);
             preparedStatement.setInt(1, userId);
             preparedStatement.setInt(2, projekId);
             preparedStatement.setString(3, nama);
+            preparedStatement.executeUpdate();
             return true;
         } catch (Exception e) {
             e.printStackTrace();
@@ -21,28 +25,52 @@ public class TeamCtrl extends DbConnect {
         return false;
     }
 
-    public static Team getTeam(int user_id, int projek_id) {
-        query = "SELECT FROM anggaran WHERE user_id=? AND projek_id=?";
+    public static List<User> getTeamMembers(int projek_id) {
+        List<User> members = new ArrayList<>();
+        String query = "SELECT user_id FROM team WHERE projek_id=?";
+        
         try {
             getConnection();
             preparedStatement = connection.prepareStatement(query);
-            preparedStatement.setInt(1, user_id);
-            preparedStatement.setInt(2, projek_id);
+            preparedStatement.setInt(1, projek_id);
             try (ResultSet resultSet = preparedStatement.executeQuery()) {
-                if (resultSet.next()) {
-                    int userId = resultSet.getInt("user_id");
-                    int projekId = resultSet.getInt("projek_id");
-                    String teams = resultSet.getString("team");
-                    Team team = new Team(userId, projekId, teams);
-                    return team;
+                List<Integer> userIds = new ArrayList<>();
+                while (resultSet.next()) {
+                    userIds.add(resultSet.getInt("user_id"));
+                }
 
+                for (int userId : userIds) {
+                    User user = getUserById(userId);
+                    if (user != null) {
+                        members.add(user);
+                    }
                 }
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
+        return members;
+    }
 
+    private static User getUserById(int userId) {
+        String query = "SELECT * FROM user WHERE id=?";
+        try {
+            preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setInt(1, userId);
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                if (resultSet.next()) {
+                    int id = resultSet.getInt("id");
+                    String nama = resultSet.getString("Nama");
+                    String email = resultSet.getString("email");
+                    String password = resultSet.getString("password");
+                    String profil_path = resultSet.getString("profil_path");
+
+                    return new User(id, nama, email, password, profil_path);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         return null;
-
     }
 }
--- Gradle/app/src/main/java/gradle/views/TeamCard.java
+++ Gradle/app/src/main/java/gradle/views/TeamCard.java
@@ -1,17 +1,48 @@
 package gradle.views;
 
-import gradle.models.Anggaran;
+import gradle.controllers.TeamCtrl;
 import gradle.models.Proyek;
+import gradle.models.User;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
-import java.util.ArrayList;
+
 import java.util.List;
 
-public class TeamCard extends VBox{
-    public TeamCard(Proyek projek){
+public class TeamCard extends VBox {
+    public TeamCard(Proyek projek) {
         this.getStyleClass().add("cardProjek");
-        
-        Label totalBudgetLabel = new Label("Budget : Rp. ");
-        this.getChildren().add(totalBudgetLabel);
+
+        Label teamNameLabel = new Label(projek.getMyTeam());
+        teamNameLabel.getStyleClass().add("h3");
+        this.getChildren().add(teamNameLabel);
+
+        List<User> members = TeamCtrl.getTeamMembers(projek.getProjekId());
+        addTeamMembers(members);
+    }
+
+    private void addTeamMembers(List<User> members) {
+        for (User member : members) {
+            this.getChildren().add(createMemberCard(member));
+        }
+    }
+
+    private HBox createMemberCard(User member) {
+        HBox memberCard = new HBox(5);
+        memberCard.getStyleClass().add("memberCard");
+
+        ImageView profileImageView = new ImageView();
+        Image profileImage = new Image(getClass().getResourceAsStream("/icons/user-circle-regular-24.png"));
+        profileImageView.setImage(profileImage);
+        profileImageView.setFitHeight(20);
+        profileImageView.setFitWidth(20);
+
+        Label memberLabel = new Label(member.getNama());
+        memberLabel.getStyleClass().add("h4");
+
+        memberCard.getChildren().addAll(profileImageView, memberLabel);
+        return memberCard;
     }
 }
